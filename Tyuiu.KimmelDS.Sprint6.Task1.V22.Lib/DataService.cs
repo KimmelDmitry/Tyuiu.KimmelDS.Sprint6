53ed2e7dd044412e247f329019301314c6ab057d
EvilHui <Ukrumoski@gmail.com>
2024-12-23 17:03:04 +0500

    task 1 here go

---
+++ Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib
+{
+    public class DataService : ISprint6Task1V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            var list = new List<double>();
+            for (int x = startValue; x < stopValue; x++)
+            {
+                var division = Math.Cos(x) + 1;
+                list.Add(division == 0 ? 0 : 3 * x + 2 - ((2 * 2 - x) / division));
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint6.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
