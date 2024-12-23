d7f1ee73e391a155ddb6a6a7e550108248143dc9
EvilHui <Ukrumoski@gmail.com>
2024-12-23 17:08:12 +0500

    help me

--- Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
@@ -10,7 +10,8 @@ namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib
             for (int x = startValue; x < stopValue; x++)
             {
                 var division = Math.Cos(x) + 1;
-                list.Add(division == 0 ? 0 : 3 * x + 2 - ((2 * 2 - x) / division));
+                double f = division == 0 ? 0 : 3 * x + 2 - (2 * 2 - x) / division;
+                list.Add(Math.Round(f, 2));
             }
             return list.ToArray();
         }
