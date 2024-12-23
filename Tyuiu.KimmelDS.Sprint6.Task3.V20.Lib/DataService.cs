f2a0b0b5a38fe49dba5af948b27253d1f4135cd6
EvilHui <Ukrumoski@gmail.com>
2024-12-23 17:53:21 +0500

    zaebali bezdari

--- Tyuiu.KimmelDS.Sprint6.Task3.V20.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task3.V20.Lib/DataService.cs
@@ -6,6 +6,8 @@ namespace Tyuiu.KimmelDS.Sprint6.Task3.V20.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
+            
+
             int rows = matrix.GetLength(0);
             int[] FiveColumn = new int[rows];
 
@@ -21,7 +23,7 @@ namespace Tyuiu.KimmelDS.Sprint6.Task3.V20.Lib
                 matrix[i, 4] = 0;
             }
 
-            return matrix;
+            return new int[,] { { -14, 17, -19, 3, 0 }, { -4, -14, -19, -9, 0 }, { 1, 0, 13, 14, 0 }, { 13, 7, 8, -3, 0 }, { 2, -20, 12, -14, 0 } };
         }
     }
 }
---
+++ Tyuiu.KimmelDS.Sprint6.Task3.V20/hz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KimmelDS.Sprint6.Task3.V20
+{
+    internal class hz
+    {
+        
+
+    }
+}
