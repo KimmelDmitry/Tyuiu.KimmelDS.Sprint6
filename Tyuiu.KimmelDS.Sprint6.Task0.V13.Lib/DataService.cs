40fc19ed6e82ee193a0e71d5496ef44605a90a62
EvilHui <Ukrumoski@gmail.com>
2024-12-23 16:47:53 +0500

    pzdc kak vi eto shavali)?)X)X)X)X)))

--- Tyuiu.KimmelDS.Sprint6.Task0.V13.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task0.V13.Lib/DataService.cs
@@ -4,6 +4,6 @@ namespace Tyuiu.KimmelDS.Sprint6.Task0.V13.Lib
 {
     public class DataService : ISprint6Task0V13
     {
-        public double Calculate(int x) => (x * x * x) / (x * x - 1);
+        public double Calculate(int x) => (double)(x * x * x) / (double)(x * x - 1);
     }
 }
