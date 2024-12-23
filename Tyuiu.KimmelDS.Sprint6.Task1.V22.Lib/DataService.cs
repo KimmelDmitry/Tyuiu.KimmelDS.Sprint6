00b12ea6ce73334ce1439cc76fafdac76b3a19f1
EvilHui <Ukrumoski@gmail.com>
2024-12-23 17:11:59 +0500

    fasdff

--- Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib
             for (int x = startValue; x < stopValue; x++)
             {
                 var division = Math.Cos(x) + 1;
-                double f = division == 0 ? 0 : 3 * x + 2 - (2 * 2 - x) / division;
+                double f = division == 0 ? 0 : 3 * x + 2 - ((2 * 2 - x) / division);
                 list.Add(Math.Round(f, 2));
             }
             return list.ToArray();
