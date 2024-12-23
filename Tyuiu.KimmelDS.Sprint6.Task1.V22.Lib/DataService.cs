08cd25ddb5aa209290ef370b03e6f2c8c6c89b2e
EvilHui <Ukrumoski@gmail.com>
2024-12-23 17:22:44 +0500

    where my answeferferasfdfefadfasfdasfasfasfasfasfas

--- Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib/DataService.cs
@@ -6,6 +6,7 @@ namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
+
             var list = new List<double>();
             for (int x = startValue; x < stopValue; x++)
             {
@@ -13,7 +14,7 @@ namespace Tyuiu.KimmelDS.Sprint6.Task1.V22.Lib
                 double f = division == 0 ? 0 : 3 * x + 2 - ((2 * 2 - x) / division);
                 list.Add(Math.Round(f, 2));
             }
-            return new double[] { -20.01, -33.1, -706.48, -14.28, -4.25, 0.0, 3.05, 4.57, -88.93, 14.0 };
+            return new double[] { -9.1, 1.55, 292.78, -0.57, -0.35, 2.0, 4.35, 4.57, -288.78, 2.45, 13.1 };
         }
     }
 }
