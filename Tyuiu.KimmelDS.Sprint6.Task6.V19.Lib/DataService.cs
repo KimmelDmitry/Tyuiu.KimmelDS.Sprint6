af57d8468b6e20006d68a0ae6ae97396a5d5946c
EvilHui <Ukrumoski@gmail.com>
2024-12-24 20:10:51 +0500

    fix dll

--- Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
@@ -5,11 +5,8 @@ namespace Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib
 {
     public class DataService : ISprint6Task6V19
     {
-        public string CollectTextFromFile(string str, string path)
-        {
-            var res = File.ReadAllText(path).Split(new char[] { ' ' }).Where(word => word.Contains("l")).ToString();
-            return res;
-        }
+        public string CollectTextFromFile(string path)
+            => File.ReadAllText(path).Split(new char[] { ' ' }).Where(word => word.Contains("l")).ToString();
     }
 }
 
