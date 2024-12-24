a1bc4d355dba4cb4c5af9fb65fc09d23295671e7
EvilHui <Ukrumoski@gmail.com>
2024-12-24 20:15:25 +0500

    forget about string.join

--- Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
@@ -6,7 +6,10 @@ namespace Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib
     public class DataService : ISprint6Task6V19
     {
         public string CollectTextFromFile(string path)
-            => File.ReadAllText(path).Split(new char[] { ' ' }).Where(word => word.Contains("l")).ToString();
+            =>  String.Join(" ", File.ReadAllText(path).Split(new char[] { ' ' })
+               .Where(word => word.Contains("l"))
+               .ToArray()
+    );
     }
 }
 
