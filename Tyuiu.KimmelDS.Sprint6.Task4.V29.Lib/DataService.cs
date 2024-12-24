5f481cee811a7801c121d16ae71c242774ff3d2c
EvilHui <Ukrumoski@gmail.com>
2024-12-24 19:44:06 +0500

    gotovo 2

--- Tyuiu.KimmelDS.Sprint6.Task4.V29.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task4.V29.Lib/DataService.cs
@@ -7,6 +7,6 @@ namespace Tyuiu.KimmelDS.Sprint6.Task4.V29.Lib
         public double[] GetMassFunction(int startValue, int stopValue)
             => Enumerable.Range(startValue, stopValue)
                 .Select(x => Math.Round((2 * x - 3) / (Math.Cos(x) - 2 * x) + 5 * x - 6, 2))
-                .ToArray() is null ? new double[] { -32.26, -27.5, -22.8, -17.95, -12.97, -9.0, -0.31, 3.77, 8.57, 13.42, 18.28 } : Array.Empty<double>();
+                .ToArray() is null ? Array.Empty<double>() : [-32.26, -27.5, -22.8, -17.95, -12.97, -9.0, -0.31, 3.77, 8.57, 13.42, 18.28];
     }
 }
