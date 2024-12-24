a936e33cefdb77b6a592203546da914c9e5c52f3
EvilHui <Ukrumoski@gmail.com>
2024-12-24 20:07:36 +0500

    test

--- Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
@@ -5,8 +5,11 @@ namespace Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib
 {
     public class DataService : ISprint6Task6V19
     {
-        public string CollectTextFromFile(string str, string path) 
-            => File.ReadAllText(path).Split(new char[] { ' ' }).Where(word => word.Contains("l")).ToString();
+        public string CollectTextFromFile(string str, string path)
+        {
+            var res = File.ReadAllText(path).Split(new char[] { ' ' }).Where(word => word.Contains("l")).ToString();
+            return res;
+        }
     }
 }
 
