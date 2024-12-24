5544153e0ed05ae8709f22679d6ea3ea205f5c21
EvilHui <Ukrumoski@gmail.com>
2024-12-24 20:17:21 +0500

    text call

--- Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KimmelDS.Sprint6.Task6.V19.Lib
     public class DataService : ISprint6Task6V19
     {
         public string CollectTextFromFile(string path)
-            =>  String.Join(" ", File.ReadAllText(path).Split(new char[] { ' ' })
+            =>  String.Join(" ", File.ReadAllText(path).Split(new char[] { ' ', '\n' })
                .Where(word => word.Contains("l"))
                .ToArray()
     );
